3736e2f64590aa5f871a1b2b36b4224fc83dc5fb
fgrig001 <fritzgrigsby@gmail.com>
2016-03-14 17:27:05 -0700

    Update

--- Simulator/Simulator.cpp
+++ Simulator/Simulator.cpp
@@ -1,3 +1,16 @@
 #include "Simulator.h"
 
 
+Sumulator::Simulator():
+	numRegisters(10),
+	fullForwarding(false),
+	registerBypassing(false),
+	branchPredictedNotTaken(false)
+{
+	MyFetch = new Fetch();	
+	MyDecode = new Decode ();
+	MyEcecute = new Execute();
+	MyMemAccess = new MemAccess();
+	MyWriteback = new Writeback();
+	registerStatus = new bool[numRegisters];
+}
--- Simulator/Simulator.h
+++ Simulator/Simulator.h
@@ -1,5 +1,7 @@
 #pragma once
 
+#include <vector>
+#include "Instruction.h"
 #include "Fetch.h"
 #include "Decode.h"
 #include "Execute.h"
@@ -9,10 +11,26 @@
 class Simulator{
 	public:
 		Simulator();
+		void addInstruction();
+		void removeInstruction(int num);
 	private:
+		// Pipline Stage Objects
 		Fetch *MyFetch;
 		Decode *MyDecode;
 		Execute *MyEcecute;
 		MemAccess *MyMemAccess;
 		Writeback *MyWriteback;
+		// Instruction Buffer 
+		vector<Instruction*> instructionBuffer;
+		// Register status array
+		bool *registerStatus;	
+		// The number of total registers
+		int numRegisters;
+		// Pipline Configuration Featur States
+		bool fullForwarding;
+		bool registerBypassing;
+		bool branchPredictedNotTaken;
+		// TODO: add these configurations (non binary options?)
+		/* - Stage where branches are taken
+		   - Stage where branches are resloved */
 };
